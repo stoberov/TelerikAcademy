9ba09809c120daac55c9b3fef7ecdd02e1f00dd1
Stoyan <stoyan.berov@gmail.com>
2015-06-22 22:00:56 +0300

    Added full 01.Functions homework

--- JavaScript - OOP/01.Functions-and-Function-Expressions/tasks/task-2.js
+++ JavaScript - OOP/01.Functions-and-Function-Expressions/tasks/task-2.js
@@ -1,13 +1,47 @@
 /* Task description */
 /*
-	Write a function that finds all the prime numbers in a range
-		1) it should return the prime numbers in an array
-		2) it must throw an Error if any on the range params is not convertible to `Number`
-		3) it must throw an Error if any of the range params is missing
-*/
+ Write a function that finds all the prime numbers in a range
+ 1) it should return the prime numbers in an array
+ 2) it must throw an Error if any on the range params is not convertible to `Number`
+ 3) it must throw an Error if any of the range params is missing
+ */
 
-function findPrimes() {
-	
+function findPrimes(from, to) {
+    var primes = [],
+        i,
+        j,
+        maxDivisor,
+        isPrime;
+    from = +from;
+    to = +to;
+
+    if (arguments.length < 2) {
+        throw new Error;
+    } else if (isNaN(from) || isNaN(to)) {
+        throw new Error;
+    }
+
+    for (i = from; i <= to; i += 1) {
+        maxDivisor = Math.sqrt(i);
+        isPrime = true;
+
+        if (i < 2) {
+            isPrime = false;
+        }
+
+        for (j = 2; j <= maxDivisor; j += 1) {
+            if (!(i % j)) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+
+    return primes;
 }
 
 module.exports = findPrimes;
